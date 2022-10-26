7f79b51e9a28713dcbdf39140ad95971da65c5ed
samer <samer_ghosn@hotmail.com>
2022-10-26 20:21:19 +0300

    fixed layout in wine component

--- src/WineComponent.js
+++ src/WineComponent.js
@@ -3,20 +3,29 @@ import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
 const WineComponent = ({ wineData }) => {
 	return (
-		<div>
+		<div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr 1fr' }}>
 			{wineData.map((elem) => {
 				return (
-					<div style={{ display: 'grid' }}>
-						<Card key={elem.id} sx={{ maxWidth: 345 }}>
+					<div style={{ padding: 10 }}>
+						<Card sx={{ maxWidth: 345 }}>
+							<CardHeader title={elem.wine} />
+
 							<CardMedia
 								component='img'
-								height={700}
+								height='400'
 								image={elem.image}
-								sx={{ gap: 6 }}
+								alt={elem.wine}
+								sx={{ padding: '16px 16px 0 16px', objectFit: 'contain' }}
 							/>
+							<CardContent>
+								<Typography variant='body2' color='text.secondary'>
+									Made in {elem.winery} winery located in {elem.location}
+								</Typography>
+							</CardContent>
 						</Card>
 					</div>
 				);
